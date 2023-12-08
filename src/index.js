36911a1588043c5144902bb574af2d36d0c82ea4
BigTogu <silvia.kenaan@gmail.com>
2023-12-08 20:44:09 +0100

    Fix import statements and update server configuration

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import express from "express";
+import express from 'express';
 //import morgan from "morgan";
-import router from "./routes/index.js";
-import { User, Group, Board, Todo } from "./db/models/index.js";
-import sequelize from "./db/config.js";
+import router from './routes/index.js';
+import sequelize from './db/config.js';
 const app = express();
 
 // Settings
-app.set("port", process.env.PORT || 8000);
-app.set("json spaces", 2);
+// eslint-disable-next-line no-undef
+app.set('port', process.env.PORT || 8000);
+app.set('json spaces', 2);
 
 app.use(express.json());
 app.use(router);
 
 sequelize
-  .sync()
-  .then(() => {
-    console.log("Sync models");
-    //Iniciando el servidor, escuchando...
-    app.listen(app.get("port"), () => {
-      console.log(`Server on port ${app.get("port")}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Connection fail", error);
-  });
+	.sync()
+	.then(() => {
+		console.log('Sync models');
+		//Iniciando el servidor, escuchando...
+		app.listen(app.get('port'), () => {
+			console.log(`Server on port ${app.get('port')}`);
+		});
+	})
+	.catch(error => {
+		console.error('Connection fail', error);
+	});
