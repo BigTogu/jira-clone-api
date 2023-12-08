42ae22e12e3a710d62f2003ab22ff266c580ceb7
BigTogu <silvia.kenaan@gmail.com>
2023-12-08 20:25:34 +0100

    Refactor database configuration and models

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,123 +1,124 @@
-import { DataTypes, Sequelize } from "sequelize";
-import sequelize from "../config.js";
-import bcrypt from "bcrypt";
+import { DataTypes, Sequelize } from 'sequelize';
+import bcrypt from 'bcrypt';
+import sequelize from '../config.js';
 
 export const User = sequelize.define(
-  "User",
-  {
-    id: {
-      type: Sequelize.UUID,
-      allowNull: false,
-      primaryKey: true,
-      defaultValue: Sequelize.UUIDV4,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    group_id: {
-      type: Sequelize.UUID,
-      allowNull: true,
-    },
-  },
-  {
-    tableName: "users",
-  }
+	'User',
+	{
+		id: {
+			type: Sequelize.UUID,
+			allowNull: false,
+			primaryKey: true,
+			defaultValue: Sequelize.UUIDV4,
+		},
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		group_id: {
+			type: Sequelize.UUID,
+			allowNull: true,
+		},
+	},
+	{
+		tableName: 'users',
+	},
 );
 
+// eslint-disable-next-line no-unused-vars
 User.beforeCreate((user, options) => {
-  return bcrypt
-    .hash(user.password, 10)
-    .then((hash) => {
-      user.password = hash;
-    })
-    .catch((err) => {
-      throw new Error(err);
-    });
+	return bcrypt
+		.hash(user.password, 10)
+		.then(hash => {
+			user.password = hash;
+		})
+		.catch(err => {
+			throw new Error(err);
+		});
 });
 User.prototype.validPassword = function (password) {
-  return bcrypt.compare(password, this.password);
+	return bcrypt.compare(password, this.password);
 };
 
 export const Group = sequelize.define(
-  "Group",
-  {
-    id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      defaultValue: DataTypes.UUID,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  { tableName: "groups" }
+	'Group',
+	{
+		id: {
+			type: DataTypes.UUID,
+			allowNull: false,
+			defaultValue: DataTypes.UUID,
+			primaryKey: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	},
+	{ tableName: 'groups' },
 );
 
 export const Board = sequelize.define(
-  "Board",
-  {
-    id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      defaultValue: DataTypes.UUID,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    group_id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      defaultValue: DataTypes.UUID,
-    },
-  },
-  { tableName: "boards" }
+	'Board',
+	{
+		id: {
+			type: DataTypes.UUID,
+			allowNull: false,
+			defaultValue: DataTypes.UUID,
+			primaryKey: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		group_id: {
+			type: DataTypes.UUID,
+			allowNull: false,
+			defaultValue: DataTypes.UUID,
+		},
+	},
+	{ tableName: 'boards' },
 );
 
 export const Todo = sequelize.define(
-  "Todo",
-  {
-    id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      defaultValue: DataTypes.UUID,
-      primaryKey: true,
-    },
-    status: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    responsable: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    task_duration: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    board_id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      defaultValue: DataTypes.UUID,
-    },
-  },
-  { tableName: "todos" }
+	'Todo',
+	{
+		id: {
+			type: DataTypes.UUID,
+			allowNull: false,
+			defaultValue: DataTypes.UUID,
+			primaryKey: true,
+		},
+		status: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		responsable: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		task_duration: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		board_id: {
+			type: DataTypes.UUID,
+			allowNull: false,
+			defaultValue: DataTypes.UUID,
+		},
+	},
+	{ tableName: 'todos' },
 );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,71 +1,71 @@
-import { Router } from "express";
-import jwt from "jsonwebtoken";
-import { User } from "../db/models/index.js";
+import { Router } from 'express';
+import jwt from 'jsonwebtoken';
+import { User } from '../db/models/index.js';
 
 const router = Router();
 
 const getToken = (username) => {
-  let token = jwt.sign(
-    {
-      username: username,
-    },
-    "secret-password",
-    { expiresIn: "48h" }
-  );
-  return token;
+	let token = jwt.sign(
+		{
+			username: username,
+		},
+		'secret-password',
+		{ expiresIn: '48h' }
+	);
+	return token;
 };
 
-router.get("/", (req, res) => {
-  res.json({
-    Title: "Hola mundo usando rutas!",
-  });
+router.get('/', (req, res) => {
+	res.json({
+		Title: 'Hola mundo usando rutas!',
+	});
 });
 
-router.get("/register", (req, res) => {
-  const { username, password } = req.body;
+router.get('/register', (req, res) => {
+	const { username, password } = req.body;
 
-  User.create({
-    username: username,
-    password: password,
-    email: username,
-  })
-    .then(() => {
-      res.json({
-        message: "Usuario creado",
-        token: getToken(username),
-      });
-    })
-    .catch((error) => {
-      res.json({
-        title: `error: ${error}`,
-      });
-    });
+	User.create({
+		username: username,
+		password: password,
+		email: username,
+	})
+		.then(() => {
+			res.json({
+				message: 'Usuario creado',
+				token: getToken(username),
+			});
+		})
+		.catch((error) => {
+			res.json({
+				title: `error: ${error}`,
+			});
+		});
 });
 
-router.post("/login", (req, res) => {
-  const { username, password } = req.body;
+router.post('/login', (req, res) => {
+	const { username, password } = req.body;
 
-  User.findOne({
-    where: {
-      username: username,
-    },
-  })
-    .then((user) => {
-      user
-        .validPassword(password)
-        .then((isValidPassword) => {
-          if (isValidPassword) return res.json({ token: getToken(username) });
-          res.json("Contraseña no válida");
-        })
-        .catch((error) => {
-          res.json({
-            error,
-          });
-        });
-    })
-    .catch(() => {
-      res.json("Usuario no encontrado");
-    });
+	User.findOne({
+		where: {
+			username: username,
+		},
+	})
+		.then((user) => {
+			user
+				.validPassword(password)
+				.then((isValidPassword) => {
+					if (isValidPassword) return res.json({ token: getToken(username) });
+					res.json('Contraseña no válida');
+				})
+				.catch((error) => {
+					res.json({
+						error,
+					});
+				});
+		})
+		.catch(() => {
+			res.json('Usuario no encontrado');
+		});
 });
 
 export default router;
