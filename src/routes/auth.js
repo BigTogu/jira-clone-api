5b1d38da818abb8943efb539f3ef9bd793ee2c72
BigTogu <silvia.kenaan@gmail.com>
2023-12-08 21:48:34 +0100

    Add authentication route for generating JWT token

---
+++ src/routes/auth.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+import claveSecreta from '../constants/configs.js';
+
+export function getToken(username) {
+	let token = jwt.sign(
+		{
+			username: username,
+		},
+		claveSecreta,
+		{ expiresIn: '48h' },
+	);
+	return token;
+}
