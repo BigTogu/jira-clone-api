91d2ec43096644b558cb80d6962a0e088e2081b8
BigTogu <silvia.kenaan@gmail.com>
2023-12-13 19:53:13 +0100

    feat: add error function

---
+++ src/statusCodes/error.js
@@ -0,0 +1,20 @@
+export class AppError extends Error {
+	constructor(message, statusCode) {
+		super(message);
+		this.statusCode = statusCode;
+		this.status = statusCode < 500 ? 'error' : 'fail';
+
+		Error.captureStackTrace(this, this.constructor);
+	}
+}
+
+// eslint-disable-next-line no-unused-vars
+export function handleError(err, req, res, next) {
+	err.status = err.status || 'fail';
+	err.statusCode = err.statusCode || 500;
+
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message,
+	});
+}
