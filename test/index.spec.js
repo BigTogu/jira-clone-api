2837cce9ed4d427d311b4f7d778ed3a54e73885f
BigTogu <silvia.kenaan@gmail.com>
2023-12-18 20:45:59 +0100

    Add registration and login tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,77 +1,106 @@
 /* eslint-disable no-undef */
 import app from '../src/app.js';
 import request from 'supertest';
+import { getAuthenticationHeader } from './scripts/setUp.js';
+import { userCorrectData } from './scripts/constants.js';
+import bcrypt from 'bcrypt';
 import { User } from '../src/db/models/index.js';
 
 describe('POST /register', () => {
 	test('should respond with a 201 status code', async () => {
-		const userData = await User.create({
+		const userData = {
 			username: 'Togu',
 			password: 'test',
 			email: 'none',
-		});
-		console.log(userData.username, 'userData');
-		console.log(User.username, User.password, 'user');
+		};
 		const response = await request(app).post('/register').send(userData);
-		console.log(response, 'response');
-		if (response.statusCode !== 201) {
-			console.log(response.body, 'response body'); // Esto mostrará el error si el estado no es 201
-		}
+
 		expect(response.statusCode).toBe(201);
-		//  User.destroy({
-		// 	where: {
-		// 		username: userData.username,
-		// 	},
-		// });
+	});
+
+	test('password is encrypted', async () => {
+		const userData = {
+			username: 'Togu',
+			password: 'test',
+			email: 'none',
+		};
+		await request(app).post('/register').send(userData);
+
+		const user = await User.findOne({
+			where: { username: 'Togu' },
+		});
+
+		const isPasswordEncrypted = await bcrypt.compare(
+			userData.password,
+			user.password,
+		);
+		expect(isPasswordEncrypted).toBe(true);
 	});
 });
 
-// 	// test('should respond with an array', async () => {
-// 	// 	const response = await request(app).get('/tasks').send();
-// 	// 	// expect(response.body).toEqual(expect.arrayContainer([]));
-// 	// 	expect(response.body).toBeInstanceOf(Array);
-// 	// });
-// });
+describe('POST /login', () => {
+	test('should respond with a 200 status code', async () => {
+		const userData = {
+			username: 'Togu',
+			password: 'test',
+			email: 'none',
+		};
+		const response = await request(app).post('/login').send(userData);
 
-// describe('POST /tasks', () => {
-// 	describe('given a title and description', () => {
-// 		const newTask = {
-// 			title: 'Test Task',
-// 			description: 'Test description',
-// 		};
+		expect(response.statusCode).toBe(200);
+	});
 
-// 		test('should respond with a 200 status code', async () => {
-// 			const response = await request(app).post('/tasks').send(newTask);
-// 			expect(response.statusCode).toBe(200);
-// 		});
+	test('should respond with a 404 status code', async () => {
+		const userDataNotFound = {
+			username: 'UsuarioNoRegistrado',
+			password: 'test',
+			email: 'none',
+		};
+		const response = await request(app).post('/login').send(userDataNotFound);
 
-// 		test('should have a content-type: application/json in header', async () => {
-// 			const response = await request(app).post('/tasks').send(newTask);
+		expect(response.statusCode).toBe(404);
+	});
+});
 
-// 			// expect(response.header['content-type'].toBe('application/json'));
-// 			expect(response.headers['content-type']).toEqual(
-// 				expect.stringContaining('json'),
-// 			);
-// 		});
+describe('GET /me', () => {
+	test('should respond with a 200 status code', async () => {
+		const headerAuthentication = await getAuthenticationHeader(
+			userCorrectData.username,
+		);
+		const response = await request(app)
+			.get('/me')
+			.set('authorization', headerAuthentication)
+			.send();
 
-// 		test('should respond with a json object containing the new task with a id', async () => {
-// 			const response = await request(app).post('/tasks').send(newTask);
-// 			expect(response.body.id).toBeDefined();
-// 		});
-// 	});
+		expect(response.statusCode).toBe(200);
+	});
 
-// 	describe('when title and description is missing', () => {
-// 		test('should respond with a 404 status code', async () => {
-// 			const fields = [
-// 				{},
-// 				{ title: 'Task Title' },
-// 				{ description: 'Test Description' },
-// 			];
+	test('should respond with a json object containing the user with a id, username and email', async () => {
+		const expectedFields = ['username', 'email', 'id'];
 
-// 			for (const body of fields) {
-// 				const response = await request(app).post('/tasks').send(body);
-// 				expect(response.statusCode).toBe(404);
-// 			}
-// 		});
-// 	});
-// });
+		const headerAuthentication = await getAuthenticationHeader(
+			userCorrectData.username,
+		);
+		const response = await request(app)
+			.get('/me')
+			.set('authorization', headerAuthentication)
+			.send();
+		expectedFields.forEach(field => {
+			expect(response.body[field]).toBeDefined();
+		});
+	});
+});
+
+describe('GET /', () => {
+	test('should respond with a 200 status code', async () => {
+		const headerAuthentication = await getAuthenticationHeader(
+			userCorrectData.username,
+		);
+		const response = await request(app)
+			.get('/')
+			.set('authorization', headerAuthentication)
+			.send();
+
+		expect(response.statusCode).toBe(200);
+	});
+});
