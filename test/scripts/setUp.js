e326298af25f384d5787fa84842636605ee0ac3f
BigTogu <silvia.kenaan@gmail.com>
2023-12-18 20:45:13 +0100

    feat: add user authentication header function before tests

--- test/scripts/setUp.js
+++ test/scripts/setUp.js
@@ -1,7 +1,19 @@
 import { User } from '../../src/db/models/index.js';
+import { getToken } from '../../src/auth';
+import { userCorrectData } from './constants.js';
 
 // eslint-disable-next-line no-undef
-beforeAll(() => {
+beforeAll(async () => {
 	console.log('base de datos de test eliminada');
 	User.truncate();
+	await User.create(userCorrectData);
 });
+
+export async function getAuthenticationHeader(username) {
+	const user = await User.findOne({
+		where: {
+			username,
+		},
+	});
+	return 'Bearer ' + getToken(user.id);
+}
